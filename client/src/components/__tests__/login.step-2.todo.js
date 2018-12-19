0a235bfcdbf8f17076e6cf5fe5395c335ecc810b
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-12-19 10:37:28 -0500

    refactored login test using react-testing-library

--- client/src/components/__tests__/login.step-2.todo.js
+++ client/src/components/__tests__/login.step-2.todo.js
@@ -1,9 +1,8 @@
 // using helpful utilities
 import React from 'react'
-import ReactDOM from 'react-dom'
 // 🐨 you'll need these:
-// import {generate} from 'til-client-test-utils'
-// import {render, fireEvent} from 'react-testing-library'
+import {generate} from 'til-client-test-utils'
+import {render, fireEvent} from 'react-testing-library'
 
 // note that til-client-test-utils is found in `client/test/til-client-test-utils`
 // note also that the client/test/setup-test-framework.js file takes care of
@@ -13,29 +12,31 @@ import Login from '../login'
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // 🐨 use generate.loginForm() here instead of assigning fakeUser to an object
-  const fakeUser = {username: 'chucknorris', password: '(╯°□°）╯︵ ┻━┻'}
+  const fakeUser = generate.loginForm()
   const handleSubmit = jest.fn()
   // 🐨 use: render(<Login onSubmit={handleSubmit} />)
   // It'll give you back an object with
   // `getByLabelText` and `getByText` functions
   // so you don't need a div anymore!
   // 💰 const {getByLabelText, getByText} = render(<Login onSubmit={handleSubmit} />)
-  const div = document.createElement('div')
-  ReactDOM.render(<Login onSubmit={handleSubmit} />, div)
+  // const div = document.createElement('div')
+  const {getByLabelText, getByText} = render(<Login onSubmit={handleSubmit} />)
+  // ReactDOM.render(<Login onSubmit={handleSubmit} />, div)
 
-  const inputs = div.querySelectorAll('input')
-  const usernameNode = inputs[0]
-  const passwordNode = inputs[1]
-  const formNode = div.querySelector('form')
-  const submitButtonNode = div.querySelector('button')
+  // const inputs = div.querySelectorAll('input')
+  const usernameNode = getByLabelText('Username')
+  const passwordNode = getByLabelText('Password')
+
+  const submitButtonNode = getByText('Submit')
 
   usernameNode.value = fakeUser.username
   passwordNode.value = fakeUser.password
 
   // Act
   // 🐨 Use fireEvent.click(submitButtonNode) instead of these two lines
-  const event = new window.Event('submit')
-  formNode.dispatchEvent(event)
+  // const event = new window.Event('submit')
+  // formNode.dispatchEvent(event)
+  fireEvent.click(submitButtonNode)
 
   // Assert
   expect(handleSubmit).toHaveBeenCalledTimes(1)
